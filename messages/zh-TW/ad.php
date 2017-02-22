b5960602f3dca0a63434b5633616b50848f04766
zacksleo <zacksleo@gmail.com>
2017-02-22 21:18:55 +0800

    add zh-TW

---
+++ messages/zh-TW/ad.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'Create' => '創建',
+    'Update' => '修改',
+    'Delete' => '刪除',
+    'View' => '查看',
+    'Active' => '激活',
+    'Inactive' => '禁用',
+
+
+    'Slug' => '唯一標識',
+    'Size' => '尺寸',
+
+    'Position ID' => '廣告位置',
+    'Name' => '名稱',
+    'Image' => '圖片',
+    'Text' => '文本',
+    'Type' => '類型',
+    'Url' => '鏈接',
+    'Status' => '狀態',
+    'Order' => '序號',
+
+    'Ad Positions' => '廣告位置',
+];
