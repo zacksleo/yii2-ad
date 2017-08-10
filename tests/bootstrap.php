46286f674319af37447fc640ed22184a83310955
monster-hunter <zhangjunwei@lianluo.com>
2017-08-10 18:13:28 +0800

    phpunit

---
+++ tests/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zjw
+ * Date: 2017/8/7
+ * Time: 下午3:52
+ */
+// ensure we get report on all possible php errors
+error_reporting(-1);
+define('YII_ENABLE_ERROR_HANDLER', false);
+define('YII_DEBUG', true);
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+Yii::setAlias('@tests', __DIR__);
+
+Yii::setAlias('@migrate', dirname(__DIR__) . '/src/migrations');
+
+Yii::setAlias('@zacksleo\yii2\ad\messages', dirname(__DIR__) . '/src/messages');
+
+Yii::setAlias('@frontend/web/uploads/galleries', __DIR__);
+
+Yii::setAlias("@web/uploads/galleries/", __DIR__);
