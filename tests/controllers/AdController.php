abd628a771364d4f9bf324462af20f27c4d49c3b
monster-hunter <1234@qq.com>
2017-08-13 13:49:09 +0800

    phpunit

---
+++ tests/controllers/AdController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace zacksleo\yii2\ad\tests\controllers;
+
+use Yii;
+use zacksleo\yii2\ad\models\Ad;
+use yii\web\Controller;
+
+class AdController extends Controller
+{
+    /**
+     * Lists all Ad models.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $data =Yii::$app->request->bodyParams;
+        $model = new Ad();
+        $model->setScenario('insert');
+        $model->load($data) && $model->save();
+        return $model;
+    }
+
+    public function actionUpdate($id)
+    {
+        $data =Yii::$app->request->bodyParams;
+        $model = $this->findModel($id);
+        $model->scenario = 'update';
+        $model->load($data) && $model->save();
+        return $model;
+    }
+
+    public function actionDelete($id)
+    {
+        return $this->findModel($id)->delete();
+    }
+
+    public function actionView($id)
+    {
+        return $this->findModel($id);
+    }
+
+    protected function findModel($id)
+    {
+        if (($model = Ad::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
